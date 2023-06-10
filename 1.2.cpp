14938992432a304ada41bff3e87af186272eee86
Carlos Henrique <65225200+CHenriqueF@users.noreply.github.com>
2023-06-10 19:37:44 -0300

    1.1->1.2 escrita no arquivo csv + adição do gitignore

--- 1.2.cpp
+++ 1.2.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <cstring>
 using namespace std;
+ofstream Saida;
 
 class Pessoa{
     private:
@@ -11,10 +12,15 @@ class Pessoa{
     public:
         Pessoa(){
             cout<< "Digite o nome da pessoa: ";
+            //getline(cin,nome);
             cin>> nome;
+            Saida<< nome<< ";";
             
             cout<< "Digite o salario da pessoa: ";
             cin>> salario;
+            Saida<< salario<< ";";
+            Saida<< "\n";
+            
         }
         string mostrar_nome(){return nome;}
         int mostrar_salario(){return salario;}
@@ -23,13 +29,15 @@ class Pessoa{
 int main(){
     int n;
 
-    cout<< "Digite quantas pessoas seram cadastradas: ";
+    Saida.open("pessoas.csv", ios:: app);
+
+    cout<< "Digite quantas pessoas serao cadastradas: ";
     cin>> n;
 
     Pessoa *p = new Pessoa[n];
 
-    cout<< "Nome da pessoa cadastrada: "<< p->mostrar_nome()<< endl;
-    cout<< "Salario da pessoa cadastrada: "<< p->mostrar_salario()<< endl;
+    //cout<< "Nome da pessoa cadastrada: "<< p->mostrar_nome()<< endl;
+    //cout<< "Salario da pessoa cadastrada: "<< p->mostrar_salario()<< endl;
 
     delete p;
     return 0;
