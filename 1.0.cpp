ca9a3f2970c4767d4e37b6edf827f0628d5a0a0c
Carlos Henrique <ferreiraeletric@hotmail.com>
2023-06-10 11:23:59 -0300

    commit inicial

---
+++ 1.0.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+using namespace std;
+/*
+class Pessoa{
+    private:
+
+    public:
+    
+};
+*/
+int main(){
+
+    cout<< "Hello world";
+    
+    return 0;
+}
